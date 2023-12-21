be7ff119b43c6ff6f1f28fb89a2ea8feaf7ff564
rogoma <rogoma007@gmail.com>
2023-12-21 00:10:51 -0300

    PizzaController2

--- app/Http/Controllers/PizzaController.php
+++ app/Http/Controllers/PizzaController.php
@@ -4,7 +4,16 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Models\Pizza;
+
 class PizzaController extends Controller
 {
-    //
+    public function index()
+    {
+        $pizzas = Pizza::all();
+
+        // return Inertia::render('Pizzas/All', [
+        //     'pizzas' => $pizzas
+        // ]);
+    }    
 }
